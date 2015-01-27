ec615c35064d7f1c56fd87f9cb248ac8256134ac
Nestor <Nestor@Nestors-MacBook.local>
2015-01-27 15:29:47 +0800

    Initial Commit

---
+++ NESXGridLayoutViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NESXGridLayoutViewDemo
+//
+//  Created by Nestor on 15/1/27.
+//  Copyright (c) 2015年 NesTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
